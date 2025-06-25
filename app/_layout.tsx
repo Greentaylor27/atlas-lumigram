63bff674bdaff2da48fa2b41967078a9a9d49604
Greentaylor27 <greentaylor18@gmail.com>
2025-06-25 18:35:58 -0500

    So I've spent probably 5 hours trying to get gestures to work but for some reason it wasn't working will ask for help with it tomorrow.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="index" options={{ headerShown: false }} />
-    <Stack.Screen name="RegisterPage" options={{ headerShown: false }} />
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-  </Stack>
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="RegisterPage" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
