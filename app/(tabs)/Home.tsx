c97f441e7e0a0334929a6fad1f4512bee17df4e7
Greentaylor27 <greentaylor18@gmail.com>
2025-06-24 22:31:49 -0500

    Beginning stages of the home feed.

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,9 +1,31 @@
-import { Text, View } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 
 export default function Home() {
-    return (
-        <View>
-            <Text>Home</Text>
-        </View>
-    )
+  return (
+      <View style={styles.container}>
+        {/* This is where the images goes */}
+      </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerBox: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: 16,
+    paddingTop: 32,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    fontSize: 20,
+  },
+  imageElement: {
+    borderRadius: 5,
+  }
+})
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,62 @@
-import { Tabs } from 'expo-router';
+import { Tabs, usePathname } from 'expo-router';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { View, Text, StyleSheet, Pressable, Alert } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
 
 export default function TabLayout() {
+  const pathname = usePathname();
+  const route = pathname.split('/').pop();
+
+  const titles: Record<string, string> = {
+    Home: 'Home Feed',
+    Add: 'Add Post',
+    Favorites: 'Favorites',
+  };
+
+  const headerTitle = titles[route || ''] || '';
+
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen
-        name='Home'
-        options={{
-          title: 'Home',
-          headerShown: false,
-          tabBarIcon: () => (
-            <AntDesign name="home" size={24} color="black" />
-          )
-        }}
-      />
-    </Tabs>
+    <View style={{ flex: 1 }}>
+      {/* Custom Header */}
+      <View style={styles.headerBox}>
+        <Text style={styles.headerText}>{headerTitle}</Text>
+        <Pressable
+          style={{ padding: 6 }}
+          onPress={() => Alert.alert('You are logging out!')}
+        >
+          <Entypo name="log-out" size={20} color="#1DD2AF" />
+        </Pressable>
+      </View>
+      <Tabs screenOptions={{ headerShown: false }}>
+        <Tabs.Screen
+          name='Home'
+          options={{
+            headerShown: false,
+            tabBarIcon: () => (
+              <AntDesign name="home" size={24} color='black' />
+            )
+          }}
+        />
+      </Tabs>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerBox: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: 16,
+    paddingTop: 32,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    fontSize: 20,
+  },
+})
