940d15008206acbaa01177e7166fa874cb6a365b
Greentaylor27 <greentaylor18@gmail.com>
2025-06-24 21:25:20 -0500

    Finally updated to finally remove (tabs) as the header also added a new package to add icons to the tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,19 @@
 import { Tabs } from 'expo-router';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function TabLayout() {
-    return (
-        <Tabs>
-            <Tabs.Screen name='Home' options={{ headerShown: false}} />
-        </Tabs>
-    )
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name='Home'
+        options={{
+          title: 'Home',
+          headerShown: false,
+          tabBarIcon: () => (
+            <AntDesign name="home" size={24} color="black" />
+          )
+        }}
+      />
+    </Tabs>
+  )
 }
